defa8b31b6c611fe4c02aabb071586c57d329b54
Suchetamon27 <suchetamonbol@gmail.com>
2025-11-15 14:54:56 +0530

    Update FloatingSocials.jsx

--- src/components/FloatingSocials.jsx
+++ src/components/FloatingSocials.jsx
@@ -1,9 +1,9 @@
 export default function FloatingSocials() {
 return (
 <div className="fixed left-6 bottom-6 flex flex-col gap-4 text-xl">
-<a href="https://github.com" className="hover:text-pink-500">🐱‍💻</a>
-<a href="https://linkedin.com" className="hover:text-pink-500">💼</a>
-<a href="https://instagram.com" className="hover:text-pink-500">📸</a>
+<a href="https://github.com" className="hover:text-pink-500"></a>
+<a href="https://linkedin.com" className="hover:text-pink-500"></a>
+<a href="https://instagram.com" className="hover:text-pink-500"></a>
 </div>
 );
 }
