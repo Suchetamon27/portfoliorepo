bbcb1022f4f01bf1959f601eccdd4c411aab7060
Suchetamon27 <suchetamonbol@gmail.com>
2025-11-15 10:54:35 +0530

    Update ThemePicker.jsx

--- src/components/ThemePicker.jsx
+++ src/components/ThemePicker.jsx
@@ -1,1 +1,9 @@
-
+export default function ThemePicker() {
+return (
+<div className="fixed top-1/2 right-6 glass p-4 rounded-xl flex flex-col gap-4">
+<button className="w-8 h-8 bg-blue-500 rounded-full"></button>
+<button className="w-8 h-8 bg-pink-500 rounded-full"></button>
+<button className="w-8 h-8 bg-green-500 rounded-full"></button>
+</div>
+);
+}
