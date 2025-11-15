c1f1dc8ede6c44eac285add0ac3cf7fe2ad499b3
Suchetamon27 <suchetamonbol@gmail.com>
2025-11-15 11:47:29 +0530

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,1 +1,7 @@
-
+export default function Loader() {
+return (
+<div className="fixed inset-0 flex items-center justify-center bg-black text-white text-3xl z-[999] animate-pulse">
+Loading...
+</div>
+);
+}
