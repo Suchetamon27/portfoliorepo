a64d7f0958323a80c52c782eab05fda25fba4ede
Suchetamon27 <suchetamonbol@gmail.com>
2025-11-15 11:16:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,39 @@
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Chatbot from "./components/Chatbot";
 import FloatingSocials from "./components/FloatingSocials";
 import ThemePicker from "./components/ThemePicker";
 import Loader from "./components/Loader";
-import ParticlesBG from "./effects/ParticlesBG";
-import CustomCursor from "./effects/CustomCursor";
 
+import CustomCursor from "./effects/CustomCursor";
+import ParticlesBG from "./effects/ParticlesBG";
 
 export default function App() {
-return (
-<div className="relative overflow-hidden">
-<Loader />
-<ParticlesBG />
-<CustomCursor />
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1500);
+  }, []);
+  if (loading) return <Loader />;
 
-<Navbar />
-<ThemePicker />
-
-
-<section id="home">
-<Hero />
-</section>
-
-
-<section id="projects">
-<Projects />
-</section>
-
-
-<section id="skills">
-<Skills />
-</section>
-
-
-<FloatingSocials />
-<Chatbot />
-</div>
-);
+  return (
+    <div className="relative overflow-hidden">
+      <ParticlesBG />
+      <CustomCursor />
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Skills />
+      <Chatbot />
+      <FloatingSocials />
+      <ThemePicker />
+    </div>
+  );
 }
+  
+
